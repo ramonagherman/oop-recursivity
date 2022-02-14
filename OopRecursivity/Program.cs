3635b0261c411fc6061b464e8e96ae04e7485677
Ramona Gherman <rgherman9128@cosmoconsult.com>
2022-02-14 16:48:54 +0200

    OopRecursivity

---
+++ OopRecursivity/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace OopRecursivity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            int n = ReadNumber("Element = ");
+
+            int[] array = new int[n];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = ReadNumber($"Element at index {i}=");
+            }
+
+            int sum = RecursivityHelper.sumArray(array, n);
+            int min = RecursivityHelper.minArray(array, n);
+
+            Console.WriteLine($"Summ is: {sum}");
+            Console.WriteLine($"Min is: {min}");
+        }
+        static int ReadNumber(string label)
+        {
+            Console.Write(label);
+            string value = Console.ReadLine();
+            int valueAsNumber = Convert.ToInt32(value);
+            return valueAsNumber;
+        }
+    }
+}
---
+++ OopRecursivity/RecursivityHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopRecursivity
+{
+    public static class RecursivityHelper
+    {
+        public static int minArray(int[] array, int element)
+        {
+            if (element == 1) return array[0];
+            else return Math.Min(array[element - 1], minArray(array, element - 1));
+        }
+
+        public static int sumArray(int[] array, int element)
+        {
+            if (element == 0) return 0;
+            else return (array[element - 1] + sumArray(array, element - 1));
+        }
+    }
+}
